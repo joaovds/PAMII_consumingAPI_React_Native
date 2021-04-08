9e2b01da1eb65034f8e12080fe202834ec22394f
João Victor da Silva <jv782063@gmail.com>
2021-04-08 14:20:35 -0300

    create home page layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Container, Input, ButtonContainer, ButtonText, Title } from './styles';
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <Title>Home</Title>
-    </Container>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <Container>
+        <Title>Movie API</Title>
+
+        <Input
+          placeholder="Enter the name of the movie"
+          autoCorrect={false}
+          placeholderTextColor='#6B7280'
+        />
+
+        <ButtonContainer>
+          <ButtonText>
+            Search
+          </ButtonText>
+        </ButtonContainer>
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,8 +4,40 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 30px;
 `;
 
 export const Title = styled.Text`
-  color: #fff;
+  color: #047857;
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 50px;
+`;
+
+export const Input = styled.TextInput`
+  width: 100%;
+  background-color: #121212;
+  color: #E5E7EB;
+  border-radius: 10px;
+  margin-bottom: 8px;
+  padding: 15px 22px;
+  font-size: 16px;
+
+  border: 1px solid #6B7280;
+  margin-bottom: 20px;
+`;
+
+export const ButtonContainer = styled.TouchableOpacity`
+  width: 100%;
+  border-radius: 10px;
+  padding: 15px 22px;
+
+  background-color: #111827;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 16px;
+  text-align: center;
+  font-weight: 600;
+  color: #4B5563;
 `;
